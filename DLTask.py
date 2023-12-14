e3c9f7484f28df8f9d51c5c126a0eee357324a9f
Jorrit Leysen <91262430+JorritLeysen@users.noreply.github.com>
2023-12-14 16:23:50 +0100

    Update DLTask.py

--- DLTask.py
+++ DLTask.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.utils import image_dataset_from_directory
 from tensorflow.keras.models import load_model
 import numpy as np
 from sklearn.metrics import confusion_matrix
